13a7e106053762b3905d1bae7a79748ab2f5268d
delanni <delanni.alex@gmail.com>
2013-11-20 00:07:42 +0100

    UI Frame proto

--- PocsKft/PocsKft/App_Start/BundleConfig.cs
+++ PocsKft/PocsKft/App_Start/BundleConfig.cs
@@ -38,6 +38,13 @@ namespace PocsKft
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/heribolz").Include(
+                "~/Scripts/appMain.js")
+                );
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular.js"));
         }
     }
 }
