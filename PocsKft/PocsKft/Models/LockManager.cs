80a34c0689574039764360e5da83f0b709524635
levios <levi.angyal@gmail.com>
2013-12-02 23:52:27 +0100

    lock

--- PocsKft/PocsKft/Models/LockManager.cs
+++ PocsKft/PocsKft/Models/LockManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -10,7 +11,7 @@ namespace PocsKft.Models
         private static volatile LockManager instance;
         private static object syncRoot = new Object();
         private LockManager() { }
-        public static LockManager LockManagerInstance
+        public static LockManager Instance
         {
             get
             {
@@ -20,6 +21,87 @@ namespace PocsKft.Models
                         instance = new LockManager();
                 }
                 return instance;
+            }
+        }
+
+
+        public bool DoesUserHaveLockOnDocument(int userId, int documnetId)
+        {
+            using (UsersContext ct = new UsersContext())
+            {
+                Lock l = ct.Locks.Where(i => i.UserId == userId && i.FolderOrDocumentId == documnetId)
+            .FirstOrDefault();
+                if (l != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public bool DoesAybodyButUserHaveLockOnDocument(int userId, int documnetId)
+        {
+            using (UsersContext ct = new UsersContext())
+            {
+                Lock l = ct.Locks.Where(i => i.UserId != userId && i.FolderOrDocumentId == documnetId)
+            .FirstOrDefault();
+                if (l != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public bool DoesAybodyHaveLockOnDocument(int documnetId)
+        {
+            using (UsersContext ct = new UsersContext())
+            {
+                Lock l = ct.Locks.Where(i => i.FolderOrDocumentId == documnetId)
+            .FirstOrDefault();
+                if (l != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public void AcquireLockOnDocument(int userId, int documnetId)
+        {
+            using (UsersContext ct = new UsersContext())
+            {
+                if (ct.Locks.Where(i => i.UserId == userId && i.FolderOrDocumentId == documnetId)
+                    .FirstOrDefault() == null)
+                {
+                    ct.Locks.Add(new Lock
+                    {
+                        UserId = userId,
+                        FolderOrDocumentId = documnetId
+                    });
+
+                    Document d = DocumentManager.Instance.GetDocumentById(userId);
+                    d.Locked = true;
+                    d.LockedByUserId = documnetId;
+                    ct.Entry(d).State = EntityState.Modified;
+
+                    ct.SaveChanges();
+                }
+            }
+        }
+
+        public void ReleaseLockOnDocument(int userId, int documnetId)
+        {
+            using (UsersContext ct = new UsersContext())
+            {
+                Lock l = ct.Locks.Where(i => i.UserId == userId && i.FolderOrDocumentId == documnetId)
+                            .FirstOrDefault();
+                if (l != null)
+                {
+                    ct.Locks.Remove(l);
+
+                    ct.SaveChanges();
+                }
             }
         }
     }
